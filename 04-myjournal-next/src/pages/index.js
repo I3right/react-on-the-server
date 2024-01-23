4aed2ca2c6356d425bea301fc9da94e8bb7561ee
jirayuka@playwork.co.th <jirayuka@playwork.co.th>
2024-01-23 15:32:34 +0700

    before change mac

--- 04-myjournal-next/src/pages/index.js
+++ 04-myjournal-next/src/pages/index.js
@@ -1,14 +1,33 @@
-import { Typography } from "@mui/joy";
+import { ArticleList } from "@/components/ArticleList";
+import { db } from "@/db";
+import { Box, Button, Typography } from "@mui/joy";
 import Container from "@mui/joy/Container";
+
 import Head from "next/head";
 
-export default function ArticleListPage() {
+export default function ArticleListPage({data}) {
   return (
     <Container sx={{ py: 2 }}>
-      <Head>
-        <title>My Journal</title>
-      </Head>
-      <Typography variant="h1">Article List Page</Typography>
+      <Box textAlign="right" mb={2}>
+        <Button component="a" href="/add">
+          Add article
+        </Button>
+      </Box>
+      <ArticleList data={data} />
     </Container>
   );
 }
+
+export const getServerSideProps = async () => {
+  const data = await db.listArticles()
+
+  return{
+    props:{
+      data: data.map((item) => ({
+        ...item,
+        createdAt: item.createdAt.toISOString()
+      }))
+    }
+  }
+}
+
